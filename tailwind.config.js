dddc63fc43a9aaa46cecbc051b5e8ff63b155d75
Milos Djakovic <milos.g.djakovic@gmail.com>
2021-01-11 00:45:07 +0100

    Tailwind purge configuration tweaked.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,20 @@
 module.exports = {
-  purge: [
-    './src/**/*.html',
-    './src/**/*.svelte'
-  ],
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        'fluo-green' : 'rgb(47, 250, 145)'
+        "fluo-green": "rgb(47, 250, 145)",
       },
       boxShadow: {
-        'bottom-fluo': '0 2px 0 rgb(47, 250, 145)',
-        'bottom-light': '0 2px 0 #f7fafc'
-      }
+        "bottom-fluo": "0 2px 0 rgb(47, 250, 145)",
+        "bottom-light": "0 2px 0 #f7fafc",
+      },
     },
   },
   variants: {
-      textColor: ['responsive', 'hover', 'focus', 'group-hover'],
-      borderColor: ['responsive', 'hover', 'focus', 'group-hover'],
-      margin: ['responsive', 'first']
+    textColor: ["responsive", "hover", "focus", "group-hover"],
+    borderColor: ["responsive", "hover", "focus", "group-hover"],
+    margin: ["responsive", "first"],
   },
   plugins: [],
-}
+};
